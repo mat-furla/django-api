5eeb2630e000c1fad9b8438247e9f11738b4c73b
Matheus Furlanetto <matfurla79@gmail.com>
2021-11-23 18:46:00 -0300

    teste

--- users/models.py
+++ users/models.py
@@ -3,7 +3,9 @@ from django.db import models
 
 
 class User(models.Model):
-    email = models.EmailField(unique=True, blank=False, max_length=254, editable=False)
+    email = models.EmailField(unique=True, blank=False,
+                              max_length=254)
+    password = models.CharField(blank=False, max_length=254)
     first_name = models.CharField(blank=False, max_length=254)
     last_name = models.CharField(blank=False, max_length=254)
     birth_date = models.DateField(blank=False)
--- users/serializers.py
+++ users/serializers.py
@@ -1,12 +1,22 @@
+from enum import unique
 from rest_framework import serializers
+from django.contrib.auth.hashers import make_password
 from .models import User
 
+import uuid
+
+
 class UserSerializer(serializers.ModelSerializer):
-    email = serializers.EmailField(max_length=254)
-    first_name = serializers.CharField(max_length=254)
-    last_name = serializers.CharField(max_length=254)
-    birth_date = serializers.DateField()
+    password = serializers.CharField(write_only=True, allow_blank=True)
 
     class Meta:
         model = User
         fields = ('__all__')
+
+    def create(self, validated_data):
+        if validated_data.get('password') == "":
+            validated_data['password'] = make_password(str(uuid.uuid4()))
+        else:
+            validated_data['password'] = make_password(
+                validated_data.get('password'))
+        return super(UserSerializer, self).create(validated_data)
